6b7c823975c86fd83d3db6d9781cf48e085a5ff4
Thomas Deconinck <thomas.d@colisweb.com>
2016-12-06 13:29:37 +0100

    es6 refactor

--- server.js
+++ server.js
@@ -35,31 +35,32 @@ app.get('/source/*', (req, res) => {
   const folderPath = path.resolve(__dirname, folderPathParams)
 
   fs.stat(folderPath, function(err, stats) {
-    var range = req.headers.range;
+    const range = req.headers.range
 
     if (!range) {
-     // 416 Wrong range
-     return res.sendStatus(416);
+      // 416 Wrong range
+     return res.sendStatus(416)
     }
-    var positions = range.replace(/bytes=/, "").split("-");
-    var start = parseInt(positions[0], 10);
-    var total = stats.size;
-    var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-    var chunksize = (end - start) + 1;
+    const positions = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(positions[0], 10)
+    const total = stats.size
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1
+    const chunksize = (end - start) + 1
 
     res.writeHead(206, {
-      "Content-Range": "bytes " + start + "-" + end + "/" + total,
+      "Content-Range": `bytes ${start}-${end}/${total}`,
       "Accept-Ranges": "bytes",
       "Content-Length": chunksize,
       "Content-Type": "video/mp4"
-    });
+    })
 
-    var stream = fs.createReadStream(folderPath, { start: start, end: end })
-      .on("open", function() {
-        stream.pipe(res);
-      }).on("error", function(err) {
-        res.end(err);
-      });
+    const stream = fs
+      .createReadStream(folderPath, {
+        start,
+        end
+      })
+      .on("open", () => stream.pipe(res))
+      .on("error", err => res.end(err))
   })
 })
 
